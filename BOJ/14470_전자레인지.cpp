03742bb87faa0bbc4656bbc1e67d162a4ce11671
LeeYunSung <dldbstjd6350@naver.com>
2021-07-08 02:19:30 +0900

    [chg] 14470_전자레인지_코드수정

--- BOJ/14470_전자레인지.cpp
+++ BOJ/14470_전자레인지.cpp
@@ -21,9 +21,6 @@ int calculateMeatWearm(int meatTemperation, int meatTargetTemperation, int iceOn
 	if (meatTemperation < 0) {
 		totalTime += -meatTemperation * iceOneTemperationUpTime;
 		meatTemperation = 0;
-	}
-	//고기가 얼어 있고 온도가 정확히 0℃일 때 : 얼어 있지 않은 상태로 만드는(해동하는) 데 D초가 걸린다.
-	if (meatTemperation == 0) {
 		totalTime += fromIceToNotIceMeatTime;
 	}
 	//고기가 얼어 있지 않을 때 : 온도가 E초에 1℃씩 오른다.
