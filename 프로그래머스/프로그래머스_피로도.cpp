8f06b831363b041b2063a1851f2652774627f80c
LeeYunSung <dldbstjd6350@naver.com>
2023-04-26 01:35:39 +0900

    [chg][프로그래머스][완전탐색] 피로도 DFS 풀이 개선

--- 프로그래머스/프로그래머스_피로도.cpp
+++ 프로그래머스/프로그래머스_피로도.cpp
@@ -1,33 +1,33 @@
 #include <string>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 using namespace std;
 
-int needEnergy[8];
-int useEnergy[8];
 bool isVisited[8] = { false, };
-int dungeonSize;
-int result = 0;
+int answer;
 
-void dfs(int cnt, int currentEnergy) {
-	for (int i = 0; i < dungeonSize; i++) {
-		if (!isVisited[i] && needEnergy[i] <= currentEnergy) {
+void DFS(int currentEnergy, int count, vector<vector<int>> dungeons) {
+	//현재 Energy가 0 이하면 바로 return 해서 시간 줄이기
+	if (currentEnergy <= 0) {
+		return;
+	}
+	for (int i = 0; i < dungeons.size(); i++) {
+		int minEnergy = dungeons[i][0];
+		int useEnergy = dungeons[i][1];
+		if (!isVisited[i] && currentEnergy >= minEnergy) {
+			//해당 가지에선 더이상 탐색을 안하기 위해 방문 true 처리
 			isVisited[i] = true;
-			dfs(cnt + 1, currentEnergy - useEnergy[i]);
+			//다음 깊이로 DFS 탐색
+			DFS(currentEnergy - useEnergy, count + 1, dungeons);
+			//리턴시 재탐색을 위해 방문 false처리
 			isVisited[i] = false;
 		}
 	}
-	result = max(result, cnt);
-	return;
+	answer = max(answer, count);
 }
 
-int solution(int k, vector<vector<int>> dungeons) {
-	int answer = -1;
-	dungeonSize = dungeons.size();
-	for (int i = 0; i < num; i++) {
-		needEnergy[i] = dungeons[i][0];
-		useEnergy[i] = dungeons[i][1];
-	}
-	dfs(0, k);
-	return answer = result;
+int solution(int currentEnergy, vector<vector<int>> dungeons) {
+	//현재 에너지 값과 탐색 깊이 값, dungeons 벡터를 넘김 
+	DFS(currentEnergy, 0, dungeons);
+	return answer;
 }
