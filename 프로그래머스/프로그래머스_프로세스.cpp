d5690358eaa33bfd60a52c6077edfd0340b9959b
LeeYunSung <dldbstjd6350@naver.com>
2023-06-03 23:43:52 +0900

    [chg][프로그래머스][스택/큐] 프로세스 오류 수정

--- 프로그래머스/프로그래머스_프로세스.cpp
+++ 프로그래머스/프로그래머스_프로세스.cpp
@@ -1,29 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-	vector<int> answer;
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+	int index = 0;
+	do {
+		index = max_element(priorities.begin(), priorities.end()) - priorities.begin();
+		for (int i = 0; i < index; i++) {
+			int front = priorities.front();
+			priorities.erase(priorities.begin());
+			priorities.push_back(front);
+		}
+		priorities.erase(priorities.begin());
+		answer++;
+	} while (location == priorities[index]);
 
-	//명령 갯수만큼 반복
-	for (int i = 0; i < commands.size(); i++) {
-
-		//명령어 값 보기좋게 선언
-		int beginIndex = commands[i][0];
-		int endIndex = commands[i][1];
-		int answerIndex = commands[i][2];
-
-		//beginInde ~ endIndex까지 값 추출
-		vector<int> partArray(array.begin() + beginIndex - 1, array.begin() + endIndex);
-
-		//오름차순 정렬
-		sort(partArray.begin(), partArray.end());
-
-		//K번째 수 추출
-		answer.push_back(partArray[answerIndex - 1]);
-	}
 
 	return answer;
 }
