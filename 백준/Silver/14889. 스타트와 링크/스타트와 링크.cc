34580c4ea49a8b2328feaa08fa28da4246f6d363
LeeYunSung <32055099+LeeYunSung@users.noreply.github.com>
2025-01-25 21:55:15 +0900

    [Silver I] Title: 스타트와 링크, Time: 120 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <limits.h>//INT_MAX
+#include <climits>
+#include <algorithm>
 using namespace std;
 
 void Input();
@@ -9,59 +10,41 @@ void CalculateTeamPower(vector<int>, vector<int>);
 
 const int MAX_PLAYER = 20;
 
-int player_num;
-int player_map[MAX_PLAYER][MAX_PLAYER];
-bool player[MAX_PLAYER];//팀원 선택할 때 사용할 변수
-int minimum = INT_MAX;//answer
-
-int min(int a, int b) {
-	return (a > b) ? b : a;
-}
-
-int abs(int a) {
-	return (a < 0) ? a * -1 : a;
-}
+int playerNum;
+int playerMap[MAX_PLAYER][MAX_PLAYER];
+bool player[MAX_PLAYER];
+int minimum = INT_MAX;
 
 int main() {
-	//시간초과 방지
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+	
+	cin >> playerNum;
+	for (int i = 0; i < playerNum; i++) {
+		for (int j = 0; j < playerNum; j++) {
+			cin >> playerMap[i][j];
+		}
+	}
 
-	//인원수, 능력치 입력받기
-	Input();
-
-	//인원수 2/n 씩 팀 나누기
 	DivideTeam(0, 0);
 	
-	//능력치 출력
 	cout << minimum;
 
 	return 0;
 }
 
-void Input() {
-	cin >> player_num;
-	for (int i = 0; i < player_num; i++) {
-		for (int j = 0; j < player_num; j++) {
-			cin >> player_map[i][j];
-		}
-	}
-}
+//인원수 2/n 씩 팀 나누기
+void DivideTeam(int currentPlayer, int count) {
+	if (count == playerNum / 2) {
+		vector<int> startTeam, linkTeam;
 
-void DivideTeam(int current_player, int count) {
-	if (count == player_num / 2) {
-		vector<int> start_team, link_team;
-
-		for (int i = 0; i < player_num; i++) {
-			if(player[i]) start_team.push_back(i);
-			else link_team.push_back(i);
+		for (int i = 0; i < playerNum; i++) {
+			if (player[i]) startTeam.push_back(i);
+			else linkTeam.push_back(i);
 		}
-		//팀 능력치 차이 구하기(최소 갱신)
-		CalculateTeamPower(start_team, link_team);
+		CalculateTeamPower(startTeam, linkTeam);
 		return;
 	}
 
-	for (int i = current_player; i < player_num; i++) {
+	for (int i = currentPlayer; i < playerNum; i++) {
 		if (player[i] == false) {
 			player[i] = true;
 			DivideTeam(i, count + 1);
@@ -70,19 +53,20 @@ void DivideTeam(int current_player, int count) {
 	}
 }
 
-void CalculateTeamPower(vector<int> start_team, vector<int> link_team) {
-	int start_team_power = 0, link_team_power = 0;
+//팀 능력치 차이 구하기(최소 갱신)
+void CalculateTeamPower(vector<int> startTeam, vector<int> linkTeam) {
+	int startTeamPower = 0, linkTeamPower = 0;
 
-	for (int i = 0; i < player_num/2; i++) {
-		for (int j = i + 1; j < player_num/2; j++) {
-			int start_x = start_team[i];
-			int start_y = start_team[j];
-			int link_x = link_team[i];
-			int link_y = link_team[j];
+	for (int i = 0; i < playerNum / 2; i++) {
+		for (int j = i + 1; j < playerNum / 2; j++) {
+			int startX = startTeam[i];
+			int startY = startTeam[j];
+			int linkX = linkTeam[i];
+			int linkY = linkTeam[j];
 
-			start_team_power += player_map[start_x][start_y] + player_map[start_y][start_x];
-			link_team_power += player_map[link_x][link_y] + player_map[link_y][link_x];
+			startTeamPower += playerMap[startX][startY] + playerMap[startY][startX];
+			linkTeamPower += playerMap[linkX][linkY] + playerMap[linkY][linkX];
 		}
 	}
-	minimum = min(abs(start_team_power - link_team_power), minimum);
+	minimum = min(abs(startTeamPower - linkTeamPower), minimum);
 }
